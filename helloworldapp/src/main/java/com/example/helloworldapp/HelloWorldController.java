630195ebdacc8cde7f3d89cfc9823ae12329bb8e
Ekta verma <ektav8231@gmail.com>
2025-02-26 00:21:54 +0530

    [Ekta] Add.helloworld SpringApp display in WebPage

--- helloworldapp/src/main/java/com/example/helloworldapp/HelloWorldController.java
+++ helloworldapp/src/main/java/com/example/helloworldapp/HelloWorldController.java
@@ -3,12 +3,21 @@ package com.example.helloworldapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloWorldController {
     @GetMapping("/")
     @ResponseBody
     public String hello() {
-        return "Hello from BridgeLabz!";
+        return "Hello from BridgeLabz!(UC1)";
     }
+
+    @GetMapping("/web")
+    @ResponseBody
+    public String message(Model model){
+    model.addAttribute("message");
+    return "Hello from BridgeLabz!(ModelUC2)";
+    }
+
 }
