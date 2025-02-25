0e2d9eaae29ec9437e8dd243e3b1f8ab071c5244
Ekta verma <ektav8231@gmail.com>
2025-02-25 23:54:47 +0530

    [Ekta] Add.HelloWorld SpringApp display using STS

---
+++ helloworldapp/src/main/java/com/example/helloworldapp/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.helloworldapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello from BridgeLabz!";
+    }
+}
